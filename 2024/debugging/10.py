86ff749c55e4624dd024683539164abf253c4e85
igor <igor@virtos.com.br>
2024-12-10 10:25:17 -0300

    day 10

---
+++ 2024/debugging/10.py
@@ -0,0 +1,42 @@
+input = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+
+def parse_input():
+    with open("C:/Projetos/Advent Of Code/2024/puzzle inputs/10.txt", "r") as file:
+        data = file.read()
+
+    return [list(map(int, row)) for row in data.splitlines()]
+
+GRID = parse_input()
+N, M = len(GRID), len(GRID[0])
+TRAILHEADS = [(i, j) for i in range(N) for j in range(M) if GRID[i][j] == 0]
+
+def dfs(i, j, pt2=False):
+    if GRID[i][j] == 9:
+        return 1 if pt2 else {(i, j)}
+
+    res = 0 if pt2 else set()
+
+    for ni, nj in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+        if 0 <= ni < N and 0 <= nj < M and GRID[ni][nj] == GRID[i][j] + 1:
+            if pt2:
+                res += dfs(ni, nj, True)
+            else:
+                res |= dfs(ni, nj)
+
+    return res
+
+def part_one():
+    return sum(len(dfs(i, j)) for i, j in TRAILHEADS)
+
+def part_two():
+    return sum(dfs(i, j, True) for i, j in TRAILHEADS)
+
+print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
